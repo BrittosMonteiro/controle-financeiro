65f96413e6d2469dc1c7f207fc8b295254930f07
Lucas Brittos <brittosmonteiro@gmail.com>
2021-01-29 12:15:32 -0300

    alteracao cor do container e cor da lista

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="lista">
         <p v-for="(l, index) in lista" v-bind:key="index">{{l.estabelecimento}} - {{l.custo}}</p>
     </div>
 </template>
@@ -12,3 +12,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.lista{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 200px;
+    background-color: #345;
+}
+</style>
